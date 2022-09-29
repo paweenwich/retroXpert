1e7274924fd45613ef2f51d5b829e7adf19b349e
paweenwich <paweenwich@yahoo.com>
2022-09-29 14:13:06 +0700

    can train

--- OpenNMT-py/script/USPTO50K-aug-typed/train.sh
+++ OpenNMT-py/script/USPTO50K-aug-typed/train.sh
@@ -14,7 +14,7 @@ dataset=USPTO50K-aug-typed
 
 python  ../../onmt/bin/train.py --data ../../data/${dataset}/${dataset} \
     --save_model ../../experiments/checkpoints/${dataset}/${dataset}_model \
-    --seed 42 --world_size 1 --gpu_ranks 0 --save_checkpoint_steps 10000 --keep_checkpoint 10 \
+    --seed 42 --save_checkpoint_steps 10000 --keep_checkpoint 10 \
     --train_steps 300000 --param_init 0  --param_init_glorot --max_generator_batches 32 \
     --batch_size 4096 --batch_type tokens --normalization tokens --max_grad_norm 0  --accum_count 4 \
     --optim adam --adam_beta1 0.9 --adam_beta2 0.998 --decay_method noam --warmup_steps 8000  \
